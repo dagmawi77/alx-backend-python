da68c8131f6f57e577a4fd92577af2050d41d4d9
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-10-06 09:18:00 +0300

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum  of two float numbers.
+    '''
+    return a + b
